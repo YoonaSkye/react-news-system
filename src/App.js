27849925c6be101bdec060fca3619764dbad1670
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-02-21 16:34:00 +0800

    修改反向代理配置

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
-import { useEffect } from "react";
 import "./App.css";
 
-import axios from "axios";
-
 function App() {
-  useEffect(() => {
-    axios.get().then((res) => {
-      console.log(res.data);
-    });
-  }, []);
-
   return <div className="App">App</div>;
 }
 
