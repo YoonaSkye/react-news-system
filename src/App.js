0c039fab99a14f7d7a1ddff71367693798031aa8
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-02-21 16:27:53 +0800

    初始化项目，配置sass和反向代理

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    axios.get().then((res) => {
+      console.log(res.data);
+    });
+  }, []);
+
+  return <div className="App">App</div>;
 }
 
 export default App;
