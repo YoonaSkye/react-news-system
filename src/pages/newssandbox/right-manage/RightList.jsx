0deb3e73cf9505f5e55bc1839eaa1d547a9213ef
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-03-30 17:28:07 +0800

    权限列表的创建(树形表格)

--- src/pages/newssandbox/right-manage/RightList.jsx
+++ src/pages/newssandbox/right-manage/RightList.jsx
@@ -1,5 +1,90 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Table, Tag, Button, Popover, Switch, Modal } from "antd";
+import {
+  DeleteOutlined,
+  EditOutlined,
+  ExclamationCircleOutlined,
+} from "@ant-design/icons";
+
+const { confirm } = Modal;
 
 export default function RightList() {
-  return <div>RightList</div>;
+  const [dataSource, setDataSource] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/rights?_embed=children").then((res) => {
+      // console.log(res.data);
+      setDataSource(res.data);
+    });
+  }, []);
+
+  const onChange = () => {
+    console.log("修改");
+  };
+
+  const content = (
+    <div>
+      <Switch defaultChecked onChange={onChange} />
+    </div>
+  );
+
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      render: (text) => <b>{text}</b>,
+    },
+    {
+      title: "权限名称",
+      dataIndex: "title",
+    },
+    {
+      title: "权限路径",
+      dataIndex: "key",
+      render: (text) => <Tag color="orange">{text}</Tag>,
+    },
+    {
+      title: "操作",
+      render: (item) => (
+        <div>
+          <Button
+            type="danger"
+            shape="circle"
+            icon={<DeleteOutlined />}
+            onClick={() => showConfirm()}
+          />
+          <Popover content={content} title="页面配置项" trigger="click">
+            <Button type="primary" shape="circle" icon={<EditOutlined />} />
+          </Popover>
+        </div>
+      ),
+    },
+  ];
+
+  const showConfirm = (item) => {
+    confirm({
+      title: "你确实要删除吗?",
+      icon: <ExclamationCircleOutlined />,
+      // content: "Some descriptions",
+      onOk() {
+        console.log("OK");
+      },
+      onCancel() {
+        console.log("Cancel");
+      },
+    });
+  };
+
+  return (
+    <div>
+      <Table
+        columns={columns}
+        dataSource={dataSource}
+        pagination={{
+          pageSize: 5,
+        }}
+      />
+    </div>
+  );
 }
